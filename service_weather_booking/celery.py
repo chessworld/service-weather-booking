fcfed46b31e63dd0e4013c8b9205537e910916a8
Trevin Wadu <trevinwadu@gmail.com>
2023-08-16 20:08:48 +1000

    Implement BOM integration.

--- service_weather_booking/celery.py
+++ service_weather_booking/celery.py
@@ -1,5 +1,4 @@
 import os
-from kombu import Exchange, Queue
 from celery import Celery
 from celery.schedules import crontab
 
@@ -8,11 +7,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service_weather_booking.setting
 app = Celery('service_weather_booking')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(['weather_api'])
+app.autodiscover_tasks()
 
-app.conf.task_queues = (
-    Queue('default', Exchange('default'), routing_key='default'),
-)
 
 app.conf.beat_schedule = {
     'booking-results-scheduler': {
--- service_weather_booking/settings.py
+++ service_weather_booking/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'drf_yasg',
     'weather_api',
-    'django_celery_results',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -142,12 +142,8 @@ CORS_ALLOWED_ORIGINS = [
 
 # Celery
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-
+# Logging
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
