17e18d7183f2bec840d65f10fee1718682b6cfb7
jjad0001 <jjad0001@student.monash.edu>
2023-09-11 16:24:04 +1000

    Restrict the 'rating' field in the Feedback model to values between 1 and 5.

--- weather_api/models/feedback.py
+++ weather_api/models/feedback.py
@@ -1,8 +1,16 @@
-from django.db import models
 import uuid
+
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
 
 class Feedback(models.Model):
     # Generate a UUID for id
     id                  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    rating              = models.IntegerField()
+    rating              = models.IntegerField(
+        validators=[
+            MinValueValidator(1, message="Rating must be at least 1"),
+            MaxValueValidator(5, message="Rating must be at most 5")
+        ]
+    )
     comment             = models.CharField(max_length=500, null=True, default=None)
--- weather_api/tests/test_feedback.py
+++ weather_api/tests/test_feedback.py
@@ -2,8 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 
-from ..models import Feedback
-
 
 class FeedbackTests(TestCase):
     def test_create_feedback(self):
@@ -15,3 +13,21 @@ class FeedbackTests(TestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue('id' in response.data)
+
+    def test_create_invalid_feedback_rating_outside_range(self):
+        url = reverse('feedback_create')
+        data = {
+            'rating': 10,
+            'comment': 'Great service!'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_invalid_feedback_rating_float(self):
+        url = reverse('feedback_create')
+        data = {
+            'rating': 3.5,
+            'comment': 'Great service!'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
